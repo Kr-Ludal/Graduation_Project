9ada06624de13203e95426a72c081ed1dd1dafd6
Kr_Ludal <rusals8055@naver.com>
2021-10-13 13:02:18 +0900

    프로젝트 리소스 수정 및 댓글 엑티비티 제작

---
+++ app/src/main/java/com/example/graduation_project/commentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.graduation_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class commentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+    }
+}
